d472114841a722c5426cb6bf1ddaaa9e6485ee7b
KoNsZwG <konszogr@hotmail.com>
2026-04-11 16:44:28 +0300

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
     '@nuxt/content',
     '@nuxtjs/google-fonts',
     '@nuxtjs/fontaine',
-    '@vercel/speed-insights',
+    '@vercel/speed-insights'   
   ],
   
   // Image optimization
